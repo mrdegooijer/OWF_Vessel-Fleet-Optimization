5896d412ce0ee6f95148ff3b4a50ad7380e494d0
Mischa de Gooijer <m.r.degooijer@student.tudelft.nl>
2025-06-09 19:02:28 +0200

    plotting update

--- utils/plotting.py
+++ utils/plotting.py
@@ -5,11 +5,14 @@ def plot_parts_vars(vars, params, sets):
     periods = sets['periods']
     spare_parts = sets['spare_parts']
     locations = sets['locations']
+    bases = sets['bases']
+    mother_vessels = sets['mother_vessels']
+    mv_offshore = vars['mv_offshore']
 
     reorder_level = params['reorder_level']
 
     #Create plot for each spare part at each base
-    for e in locations:
+    for e in bases:
         for s in spare_parts:
             plt.title(f"Spare Part: {s} at Locations: {e}")
             plt.xlabel("Period")
@@ -19,5 +22,22 @@ def plot_parts_vars(vars, params, sets):
             plt.plot(periods, [vars['inventory_level'][s, e, p].X for p in periods], label='Inventory Level')
             plt.plot(periods, [reorder_level[s, e] for p in periods], label='Reorder Level', linestyle='--')
             plt.legend()
+            plt.savefig(f"plots/spare_part_{s}_locations_{e}.png")
+            plt.close()
+
+    for e in mother_vessels:
+        for s in spare_parts:
+            plt.title(f"Spare Part: {s} at Locations: {e}")
+            plt.xlabel("Period")
+            plt.ylabel("Inventory Level")
+            plt.xticks(periods)
+            plt.grid()
+            plt.plot(periods, [vars['inventory_level'][s, e, p].X for p in periods], label='Inventory Level')
+            plt.plot(periods, [reorder_level[s, e] for p in periods], label='Reorder Level', linestyle='--')
+            plt.legend()
+            for p in periods:
+                if mv_offshore[e, p].X > 0:
+                    # shade the period gray
+                    plt.axvspan(p, p + 1, color='gray', alpha=0.5)
             plt.savefig(f"plots/spare_part_{s}_locations_{e}.png")
             plt.close()
