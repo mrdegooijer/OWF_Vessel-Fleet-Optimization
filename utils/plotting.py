48f7f1002b344fe8b55b342bb5e6b7f30c0a0d9a
Mischa de Gooijer <m.r.degooijer@student.tudelft.nl>
2025-06-13 12:55:19 +0200

    update to results.py and plotting.py

--- utils/plotting.py
+++ utils/plotting.py
@@ -4,7 +4,7 @@ from utils.initial_values import *
 def plot_parts_vars(vars, params, sets):
     # Plot spare parts decision variables
     periods = sets['periods']
-    periods_0 = [0]+list(periods)
+    periods_0 = [0] + list(periods)
     spare_parts = sets['spare_parts']
     locations = sets['locations']
     bases = sets['bases']
@@ -12,34 +12,26 @@ def plot_parts_vars(vars, params, sets):
     mv_offshore = vars['mv_offshore']
 
     reorder_level = params['reorder_level']
+    max_capacity = params['max_part_capacity']
 
     # Create plot for each spare part at each base
-    for e in bases:
+    for e in locations:
         for s in spare_parts:
+            plt.figure(figsize=(7.68, 5.76))
             plt.title(f"Spare Part: {s} at Locations: {e}")
             plt.xlabel("Period")
             plt.ylabel("Inventory Level")
-            plt.xticks(periods)
-            plt.grid()
-            plt.plot(periods_0, [params['initial_inventory'][e]] + [vars['inventory_level'][s, e, p].X for p in periods], label='Inventory Level')
-            plt.plot(periods, [reorder_level[s, e] for p in periods], label='Reorder Level', linestyle='--')
-            plt.legend()
-            plt.savefig(f"plots/spare_part_{s}_locations_{e}.png")
-            plt.close()
-
-    for e in mother_vessels:
-        for s in spare_parts:
-            plt.title(f"Spare Part: {s} at Locations: {e}")
-            plt.xlabel("Period")
-            plt.ylabel("Inventory Level")
-            plt.xticks(periods)
+            plt.xlim(0, 90)
+            plt.ylim(0, int(max_capacity[s, e]) + 10)
+            plt.xticks([p - 1 for p in periods[::15]] + [90])
             plt.grid()
             plt.plot(periods, [vars['inventory_level'][s, e, p].X for p in periods], label='Inventory Level')
-            plt.plot(periods, [reorder_level[s, e] for p in periods], label='Reorder Level', linestyle='--')
+            order_quantities = [vars['order_quantity'][s, e, p].X for p in periods]
+            plt.vlines(periods, ymin=0, ymax=order_quantities, label='Order Quantity', color='green',
+                       linestyles='-')
+            plt.plot(periods, [reorder_level[s, e] for p in periods], label='Reorder Level', linestyle=':', color='red')
+            plt.plot(periods, [max_capacity[s, e] for p in periods], label='Max Capacity', linestyle=':',
+                     color='orange')
             plt.legend()
-            for p in periods:
-                if mv_offshore[e, p].X > 0:
-                    # shade the period gray
-                    plt.axvspan(p, p + 1, color='gray', alpha=0.5)
-            plt.savefig(f"plots/spare_part_{s}_locations_{e}.png")
+            plt.savefig(f"plots/spare_part_{s}_locations_{e}.png", bbox_inches='tight')
             plt.close()
